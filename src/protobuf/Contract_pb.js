000544fe45d84d6ebac1855d6363bca64ea73d45
Gloria Wang <gloria@bitgo.com>
2019-10-16 22:04:00 -0700

    TRON-71: Add helper funcs to explain transaction:  *Modularize parseTransaction  *Create getRawTransferContract to extract necessary info to explain tx  *Add safeTronHexString to sanitize txhex string

--- src/protobuf/Contract_pb.js
+++ src/protobuf/Contract_pb.js
@@ -11,7 +11,7 @@ var jspb = require('google-protobuf');
 var goog = jspb;
 var global = Function('return this')();
 
-var Tron_pb = require('./Tron_pb.js');
+var Tron_pb = require('./tron_pb.js');
 goog.object.extend(proto, Tron_pb);
 goog.exportSymbol('proto.protocol.AccountCreateContract', null, global);
 goog.exportSymbol('proto.protocol.AccountPermissionUpdateContract', null, global);
